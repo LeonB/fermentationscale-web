68b30849fca608d66b7978bb227f8dcfb9cd0bf0
Leon Bogaert <leon@tim-online.nl>
2020-01-16 22:57:56 +0100

    compress dist files with brotli

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,23 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        compressionOptions: { level: 11 },
+
+        // asset: '[path].gz[query]',
+        // algorithm: 'gzip',
+        // test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        // threshold: 10240,
+        // minRatio: 0.8
+      })
+    ]
+  }
 }
