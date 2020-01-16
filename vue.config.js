1d68250c85a19efda37fd566fc0982e44231d988
Leon Bogaert <leon@tim-online.nl>
2020-01-16 23:02:38 +0100

    remove unused lines

--- vue.config.js
+++ vue.config.js
@@ -11,12 +11,6 @@ module.exports = {
         filename: '[path].br[query]',
         algorithm: 'brotliCompress',
         compressionOptions: { level: 11 },
-
-        // asset: '[path].gz[query]',
-        // algorithm: 'gzip',
-        // test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-        // threshold: 10240,
-        // minRatio: 0.8
       })
     ]
   }
